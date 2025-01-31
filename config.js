5a9cdab900ee257ca2b88e12d5fb08a9ae6e9df1
taimoor111547 <th2935285@gmail.com>
2025-01-31 12:57:29 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "fiICTCya#9zf9ZQMe68q9slvf3jMsOyhchk8ylVrM-m4XLh8q5sM",
     CAPTION: process.env.CAPTION || "кєєρ υѕιηg ʝσєℓ χм∂ вσт",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
@@ -14,9 +14,9 @@ module.exports = {
     AUTO_TYPING: process.env.AUTO_TYPING || "false",
     CURRENT_STATUS: process.env.CURRENT_STATUS || "false",
     MODE: process.env.MODE || "public",
-    AUTO_VOICE: process.env.AUTO_VOICE || "true",
-    AUTO_STICKER: process.env.AUTO_STICKER || "true",
-    AUTO_REPLY: process.env.AUTO_REPLY || "true",
+    AUTO_VOICE: process.env.AUTO_VOICE || "false",
+    AUTO_STICKER: process.env.AUTO_STICKER || "false",
+    AUTO_REPLY: process.env.AUTO_REPLY || "false",
     OWNER_IMG: process.env.OWNER_IMG || "https://files.catbox.moe/yvf7h8.png",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/yvf7h8.png",
     ALIVE_MSG: process.env.ALIVE_MSG || "*ɪ'ᴍ ᴀʟᴡᴀʏs ᴏɴʟɪɴᴇ ᴍᴏᴛʜᴇʀғᴜᴄᴋᴇʀ*",
@@ -24,7 +24,7 @@ module.exports = {
     ANTI_BAD: process.env.ANTI_BAD || "true",
     PREFIX: process.env.PREFIX || ".",
     FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
-    AUTO_REACT: process.env.AUTO_REACT || "false",
+    AUTO_REACT: process.env.AUTO_REACT || "true",
     HEART_REACT: process.env.HEART_REACT || "false",
     OWNER_REACT: process.env.OWNER_REACT || "false",
     BOT_NAME: process.env.BOT_NAME || "ᴊᴏᴇʟ ᴍᴅ ʙᴏᴛ",
